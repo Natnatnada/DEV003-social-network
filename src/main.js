aef04e95971234a53f4d5e011a689bfbf19339e6
Naomi Peña <naosan25@protonmail.com>
2023-01-31 18:48:44 -0600

    feat: se realizan routers, componentes de SPA y fondo CSS

--- src/main.js
+++ src/main.js
@@ -1,5 +1,35 @@
-// Este es el punto de entrada de tu aplicacion
+// eslint-disable-next-line import/no-cycle
+import { home } from './componentes/home.js';
+// eslint-disable-next-line import/no-cycle
+import { registro } from './componentes/registro.js';
+// eslint-disable-next-line import/no-cycle
+import { logIn } from './componentes/logIn.js';
+//import { feed } from './componentes/feed.js';
 
-import { myFunction } from './lib/index.js';
+const rootDiv = document.getElementById('root');
 
-myFunction();
+const routes = {
+  '/': home,
+  '/logIn': logIn,
+  '/registro': registro,
+};
+
+export const onNavigate = (pathname) => {
+  window.history.pushState(
+    {},
+    pathname,
+    window.location.origin + pathname,
+  );
+
+  rootDiv.removeChild(rootDiv.firstChild);
+  rootDiv.appendChild(routes[pathname]());
+};
+
+const component = routes[window.location.pathname];
+
+window.onpopstate = () => {
+  rootDiv.removeChild(rootDiv.firstChild);
+  rootDiv.append(component());
+};
+rootDiv.appendChild(component());
+
