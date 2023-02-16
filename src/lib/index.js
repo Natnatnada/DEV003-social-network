f2f55c312acd2cde8503ad0fe31ad91092fb4b60
Natalia Aspee <n.aspeedelgado@gmail.com>
2023-02-16 14:34:54 -0300

    feat se cambian funciones de archivo, se implementa signInUser para que puedan entrar usuarios registrados

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,44 @@
-// aqui exportaras las funciones que necesites
+import {
+  signInWithPopup, GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from 'firebase/auth';
 
-export const myFunction = () => {
-  // aqui tu codigo
-  console.log('Hola mundo!');
-};
+// signInWithEmailAndPassword, onAuthStateChanged ,signOut
+import { auth } from './firebase';
+
+export function entrarConGoogle(onNavigate) {
+  // Nos falta hacer el llamado de la función que es logInGoogle
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      onNavigate('/feed');
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+}
+// funcion para iniciar sesion con usuario ya creado
+export function signInUser(email, password) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
+// funcion para cerrar sesion
+export function signOff() {
+  return signOut(auth);
+}
+//onAuthStateChanged
+
