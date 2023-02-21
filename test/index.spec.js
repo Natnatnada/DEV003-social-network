f8a5295d72f54433e93110198265178b9a520d5e
Naomi Peña <naosan25@protonmail.com>
2023-02-21 08:16:06 -0600

    test:Avance UT para registro de usuario

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,42 @@
 // importamos la funcion que vamos a testear
-import { myFunction } from '../src/lib/index';
 
-describe('myFunction', () => {
-  it('debería ser una función', () => {
-    expect(typeof myFunction).toBe('function');
+import { registro } from '../src/componentes/registro';
+
+// -----------------Función de registro -----------------
+
+describe('Test registro', () => {
+  let emailInput;
+  let emailTest;
+  let passwordTest;
+  let createUserBtn;
+  beforeEach(() => {
+    document.body.appendChild(registro());
+    emailInput = querySelector.emailInput;
+  });
+
+  it('debe de registrar nuevas usuarias', () => {
+    // dadas
+    const signUpFormTest = document.createElement('div');
+    signUpFormTest.id = 'signUpTest';
+
+    document.body.append(signUpFormTest);
+
+    registro.appendChild(registro());
+    registro();
+
+    // entonces
+    const formSignIn = document.querySelector('#formSignInTest');
+
+    expect(formSignIn).not.toBeNull();
+    console.log(document.body.innerHTML);
+  });
+});
+// TODO:Crear un archivo spec por cada componente (home, registro...)
+
+// -----------------Función de SignIn -----------------
+describe('signUpForm', () => {
+  it('sould enter with a registered email', async () => {
+    const signUpForm = functionSignin('l_dev003@gmail.com', '123l_dev003');
+    await expect(signUpForm).resolves.toEqual('string');
   });
 });
