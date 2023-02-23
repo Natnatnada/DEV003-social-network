475d7b2a5445971e8e8483ddbe0fec71ffadbfb9
Natalia Aspee <n.aspeedelgado@gmail.com>
2023-02-23 15:56:11 -0300

    feat se avanzo la modularización con function signup, se completan parte de los test y logran correr pero con un fallo

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,12 +1,13 @@
 // importamos la funcion que vamos a testear
-
 // import { async } from 'regenerator-runtime';
 import { registro } from '../src/componentes/registro';
+import { signUp } from '../src/lib';
 
-jest.mock('..src/lib/firebase');
-
+jest.mock('../src/lib/firebase');
+jest.mock('../src/lib/index');
+// mock de index y firebase
 function tick() {
-  return new Promise ((resolve) => {
+  return new Promise((resolve) => {
     setTimeout(resolve, 0);
   });
 }
@@ -14,67 +15,28 @@ function tick() {
 // -----------------Función de registro -----------------
 
 describe('Test registro', () => {
-  let emailInput;
+  /* let emailInput;
   let inputPsw;
+  let createAcount; */
+  let generalErr;
   let createAcount;
-  let emailErrT;
-
   beforeEach(() => {
+    // llamado al componente registro
     document.body.appendChild(registro());
-    emailInput = document.getElementById('emailInput');
+    /* emailInput = document.getElementById('emailInput');
     inputPsw = document.getElementById('inputPsw');
-    createAcount = document.getElementById('createAccount');
-    emailErrT = document.getElementById('emailErrT');
+    createAcount = document.getElementById('createAccount'); */
+    generalErr = document.getElementById('generalErr');
+    createAcount = document.getElementById('createAcount');
   });
 
   it('debe mostrar error', async () => {
-    registro.mockImplementationOnce((email, password) => {
-      return Promise.reject(
-        new Error('Firebase:Error (auth/invalid-email).'),
-      );
-    });
-
-    inputForSend.click();
-await tick();
-    expect(emailErrT.innerHTML).toBe(
-            'Firebase:Error (auth/invalid-email).',
-            console.log('Error:' + emailErrT.innerHTML)); 
-     );
-});
-
-/* inputForEmail.value = 'labo-girl2@gmail.com';
-inputForPassword.value = '1234567';
-
-        inputForSend.click();
-await tick();
-expect(succcessMessage.innerHTML).toBe(
-'email@verify.com'
-);
-});
-});
-*/
-/* dadas
-    const signUpFormTest = document.createElement('div');
-    signUpFormTest.id = 'signUpTest';
-
-    document.body.append(signUpFormTest);
-
-    registro.appendChild(registro());
-    registro();
-
-     entonces
-    const formSignIn = document.querySelector('#formSignInTest');
-
-    expect(formSignIn).not.toBeNull();
-    console.log(document.body.innerHTML);
+    signUp.mockImplementationOnce(() => Promise.reject(
+      new Error('Firebase:Error (auth/invalid-email).'),
+    ));
+    // id signUpform
+    createAcount.click();
+    await tick();
+    expect(generalErr.textContent).toBe('Email inválido');
   });
 });
- TODO:Crear un archivo spec por cada componente (home, registro...)
-
- -----------------Función de SignIn -----------------
-describe('signUpForm', () => {
-  it('sould enter with a registered email', async () => {
-    const signUpForm = functionSignin('l_dev003@gmail.com', '123l_dev003');
-    await expect(signUpForm).resolves.toEqual('string');
-  }); */
-// });
