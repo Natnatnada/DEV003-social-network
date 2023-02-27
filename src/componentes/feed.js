3e5abdc1b3fb6943b027baa0dad5febe4bed67eb
Natalia Aspee <n.aspeedelgado@gmail.com>
2023-02-27 13:52:20 -0300

    se agrega importe para inicialización de firestore, se agregan elementos para guardar las tareas en el feed

--- src/componentes/feed.js
+++ src/componentes/feed.js
@@ -5,23 +5,36 @@ export const feed = (onNavigate) => {
   const div = document.createElement('div');
   const divPadre = document.createElement('div');
   const title = document.createElement('h2');
-  const inputPost = document.createElement('input');
+  const subTitle = document.createElement('h2');
+  const postForm = document.createElement('postForm');
+  const postTitle = document.createElement('input');
+  // se crea const para texto del post
+  const headerPost = document.createElement('h3');
+  const postText = document.createElement('textarea');
   const botonGuardar = document.createElement('button');
   const btnLogOut = document.createElement('button');
+
   // =====Escribimos la info dentro de los elementos=====
-  title.textContent = 'Bienvenida a tu muro';
-  inputPost.placeholder = 'Escribe aquí lo que piensas';
+
+  headerPost.textContent = 'Bienvenida a tu muro';
+  postTitle.placeholder = 'Escribe aquí lo que piensas';
   botonGuardar.textContent = 'Guardar';
   btnLogOut.textContent = 'Cerrar sesión';
   btnLogOut.className = 'botones';
+  title.className = 'titulo';
+  subTitle.className = 'subtitulo';
+  title.textContent = '<CiberFem>';
+  subTitle.textContent = 'Inspiración para programadoras';
   // ============Damos clases a los elementos==============
   divPadre.className = 'divPadre';
 
   // ==============Damos ID a los elementos===============
-  inputPost.id = 'guardarPost';
-
-  // ===========Mostramos los elementos=================
-  div.append(title, inputPost, botonGuardar, divPadre, btnLogOut);
+  postTitle.id = 'taskTitle';
+  // post text id
+  postText.id = 'textPost';
+  // se agrupan los elementos segun el form
+  postForm.append(postTitle, postText, botonGuardar);
+  div.append(title, subTitle, headerPost, postForm, divPadre, btnLogOut);
 
   btnLogOut.addEventListener('click', () => {
     signOff().then(() => {
