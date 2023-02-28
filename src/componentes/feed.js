2c5ede57f8758e2579e424ebe66b184118a9dcd3
Natalia Aspee <n.aspeedelgado@gmail.com>
2023-02-28 13:55:09 -0300

    se define funcion saveTask para guardar colecciones en firestore

--- src/componentes/feed.js
+++ src/componentes/feed.js
@@ -1,12 +1,12 @@
-import { signOff } from '../lib';
+import { signOff, saveTask } from '../lib';
 // eslint-disable-next-line no-unused-vars
 export const feed = (onNavigate) => {
 // ========Creamos los elementos de nuestro feed===========
   const div = document.createElement('div');
   const divPadre = document.createElement('div');
-  const title = document.createElement('h2');
+  const titleCiber = document.createElement('h2');
   const subTitle = document.createElement('h2');
-  const postForm = document.createElement('postForm');
+  const taskForm = document.createElement('form');
   const postTitle = document.createElement('input');
   // se crea const para texto del post
   const headerPost = document.createElement('h3');
@@ -15,26 +15,43 @@ export const feed = (onNavigate) => {
   const btnLogOut = document.createElement('button');
 
   // =====Escribimos la info dentro de los elementos=====
-
+  postTitle.type = 'text';
   headerPost.textContent = 'Bienvenida a tu muro';
   postTitle.placeholder = 'Escribe aquí lo que piensas';
   botonGuardar.textContent = 'Guardar';
   btnLogOut.textContent = 'Cerrar sesión';
   btnLogOut.className = 'botones';
-  title.className = 'titulo';
+  titleCiber.className = 'titulo';
   subTitle.className = 'subtitulo';
-  title.textContent = '<CiberFem>';
+  titleCiber.textContent = '<CiberFem>';
   subTitle.textContent = 'Inspiración para programadoras';
   // ============Damos clases a los elementos==============
   divPadre.className = 'divPadre';
 
   // ==============Damos ID a los elementos===============
-  postTitle.id = 'taskTitle';
+
+  postTitle.id = 'postTitle';
+  taskForm.id = 'taskForm';
   // post text id
-  postText.id = 'textPost';
+  postText.id = 'postText';
+  botonGuardar.id = 'botonGuardar';
+
   // se agrupan los elementos segun el form
-  postForm.append(postTitle, postText, botonGuardar);
-  div.append(title, subTitle, headerPost, postForm, divPadre, btnLogOut);
+  taskForm.append(postTitle, postText, botonGuardar);
+  div.append(titleCiber, subTitle, headerPost, taskForm, divPadre, btnLogOut);
+
+  // taskForm = document.getElementById('taskForm');
+
+  taskForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    // console.log('submitted');
+    const title = postTitle.value;
+    //console.log(postTitle.value);
+    const description = postText.value;
+    //  console.log(postText.value);
+    saveTask(title, description);
+    // taskForm.reset();
+  });
 
   btnLogOut.addEventListener('click', () => {
     signOff().then(() => {
@@ -47,5 +64,6 @@ export const feed = (onNavigate) => {
       // An error happened.
     });
   });
+
   return div;
 };
